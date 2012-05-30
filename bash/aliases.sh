44332b59c4c4df58e9aeb8dda72686c031b65c3a
Enric Joffrion-Antonetti <enric@weborama.com>
2012-05-30 17:07:06 +0200

    add bash alias

---
+++ bash/aliases.sh
@@ -0,0 +1,10 @@
+# SSH
+alias pssh="ssh -l prodsoft "
+# ACK-GREP
+alias agrep="ack-grep -i --ignore-dir=.build "
+# DZIL INSTALL
+alias dzinst="dzil install --install-command='wcpanm -n .'"
+# git pull origin devel
+alias gpod="git pull origin devel"
+# git push origin users/ejoffri
+alias gpou="git push origin users/ejoffri"
